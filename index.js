c3c2f0c39d54cdceed7a690c6a2af715c6c3787c
Akshat1sharma321 <144864684+Akshat1sharma321@users.noreply.github.com>
2024-08-05 22:37:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+// const logDiv = document.getElementById("log");
+// const stateDiv = document.getElementById("state");
+// const startBtn = document.getElementById("start-btn");
+// const stopBtn = document.getElementById("stop-btn");
+// function handleDown(e){
+//     logDiv.textContent=`key ${e.key} pressed down`;
+//     stateDiv.textContent = "Key is Down"
+
+// }
+// function handleUp(e){
+//     logDiv.textContent=`key ${e.key} pressed up`
+//     stateDiv.textContent = "Key is up"
+
+// }
+// startBtn.addEventListener("click",()=>{
+//     document.addEventListener("keydown",handleDown)
+//     document.addEventListener("keyup",handleUp)
+// })
+// stopBtn.addEventListener("click",()=>{
+//     document.addEventListener("keydown",handleDown)
+//     document.addEventListener("keyup",handleUp)
+//     logDiv.textContent = "";
+//     stateDiv.textContent = "";
+// }) 
+const logDiv = document.getElementById("log");
+const stateDiv = document.getElementById("state");
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+
+function handleDown(e) {
+    logDiv.textContent = `key ${e.key} pressed down`;
+    stateDiv.textContent = "Key is Down";
+}
+
+function handleUp(e) {
+    logDiv.textContent = `key ${e.key} pressed up`;
+    stateDiv.textContent = "Key is up";
+}
+
+startBtn.addEventListener("click", () => {
+    document.addEventListener("keydown", handleDown);
+    document.addEventListener("keyup", handleUp);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+});
+
+stopBtn.addEventListener("click", () => {
+    document.removeEventListener("keydown", handleDown);
+    document.removeEventListener("keyup", handleUp);
+    logDiv.textContent = "";
+    stateDiv.textContent = "";
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
